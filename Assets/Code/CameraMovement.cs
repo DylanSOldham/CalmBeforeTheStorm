1a3ded6cf90cab0195e6a2b343cc30bac84a2804
Dylan Oldham <dylansoldham@gmail.com>
2024-09-08 14:51:36 -0700

    Movement & Camera

--- Assets/Code/CameraMovement.cs
+++ Assets/Code/CameraMovement.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject ship;
+    public Transform shipTransform;
+
+    private Transform cameraTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraTransform = GetComponent<Transform>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Transform cameraTrans = this.gameObject.getComponent<Transform>();
-        
+        transform.LookAt(shipTransform);
+        transform.position = shipTransform.position + new Vector3(0.0f, 3.0f, -10.0f);
     }
 }
--- Assets/Code/ShipMovement.cs
+++ Assets/Code/ShipMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+    private Transform shipTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W)) {
+            shipTransform.position += 0.1f * Vector3.forward;
+        }
     }
 }
