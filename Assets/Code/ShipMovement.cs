03f0011f2d9cd3b693a9d702522261c5526ef993
Brendo <brendo.coder@gmail.com>
2024-09-09 21:14:03 -0300

    movement partially implemented

--- Assets/Code/ShipMovement.cs
+++ Assets/Code/ShipMovement.cs
@@ -9,14 +9,11 @@ public class ShipMovement : MonoBehaviour
 
     public GameObject directionObject;
 
-    public float moveSpeed = 10f; // Adjust to make it faster/slower
-    public float turnFactor = 50f; // Adjust for faster/slower rotation
-
     private Vector3 _speed = new Vector3(0, 0, 0);
-    public Vector3 maxSpeed = new Vector3(5, 0, 5);
+    public Vector3 maxSpeed = new Vector3(15, 0, 15);
 
     private Vector3 _acceleration = new Vector3(0, 0, 0);
-    public Vector3 maxAcceleration = new Vector3(10, 0, 10);
+    public Vector3 maxAcceleration = new Vector3(5, 0, 5);
 
     private float _timer = 0;
 
@@ -30,20 +27,13 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKey(KeyCode.W)) {
-        //     shipTransform.position += moveSpeed * -directionObject.transform.right * Time.deltaTime;
-        // }
-        // if (Input.GetKey(KeyCode.A))
-        // {
-        //     shipTransform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-        // }
         _timer += Time.deltaTime;
         if (_timer >= 0.2f)
         {
             _timer = 0;
-            
-            int dir = 0;
-            
+
+            var dir = 0;
+
             if (Input.GetKey(KeyCode.D))
             {
                 dir += 1;
@@ -54,30 +44,50 @@ public class ShipMovement : MonoBehaviour
                 dir -= 1;
             }
 
-            if (dir != 0)
+            // Auto stay still
+            if (dir == 0)
             {
-                if (Mathf.Abs(_acceleration.z) <= maxAcceleration.z)
-                {
-                    _acceleration.z += 3 * dir;
-                    Debug.Log(dir);
-                }
-                _speed.z += _acceleration.z;
-            }
-            else
-            {
-                Debug.Log(dir);
+                _acceleration.z = 0;
                 if (_speed.z > 0)
                 {
-                    _speed.z -= 2;
+                    var i = _speed.z - 3;
+                    _speed.z = i < 0 ? 0 : i;
                 }
                 else
                 {
-                    _speed.z += 2;
+                    var i = _speed.z + 3;
+                    _speed.z = i > 0 ? 0 : i;
                 }
             }
-            
 
+            if (dir == 1 && _acceleration.z < 0)
+            {
+                _acceleration.z = 0;
+            }
+
+            if (dir == -1 && _acceleration.z > 0)
+            {
+                _acceleration.z = 0;
+            }
+
+            if (Mathf.Abs(_acceleration.z) <= maxAcceleration.z)
+            {
+                var i = _acceleration.z + 1 * dir;
+                _acceleration.z = Mathf.Abs(i) > maxAcceleration.z ? maxAcceleration.z * dir : i;
+            }
+
+            var j = _speed.z + _acceleration.z * 2;
+            _speed.z = Mathf.Abs(j) > maxSpeed.z ? Mathf.Sign(j) * maxSpeed.z : j;
+            
+            print(_speed.z);
+            print(_acceleration.z);
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                _acceleration.x += 1;
+            }
         }
+
         var target = shipTransform.position;
         target.y = waterController.GetHeightAtPosition(target);
         shipTransform.position = target;
