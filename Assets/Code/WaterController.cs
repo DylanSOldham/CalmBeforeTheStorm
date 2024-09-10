989d88fa5a0ff3dd09e2d279b9ec581591c0dbc3
Dylan Oldham <dylansoldham@gmail.com>
2024-09-10 13:16:16 -0700

    Fixed my stupid

--- Assets/Code/WaterController.cs
+++ Assets/Code/WaterController.cs
@@ -56,9 +56,10 @@ public class WaterController : MonoBehaviour
             {
                 vertices[j * WATER_VERTEX_WIDTH + i] = new Vector3(
                     (float)i * SQUARE_SIZE - WATER_SIZE / 2.0f, 
-                    20.0f * Mathf.PerlinNoise((float)i * 0.02f + timer, (float) j * 0.02f + timer), 
+                    0.0f, 
                     (float)j * SQUARE_SIZE - WATER_SIZE / 2.0f
                 );
+                vertices[j * WATER_VERTEX_WIDTH + i].y = GetHeightAtPosition(vertices[j * WATER_VERTEX_WIDTH + i]);
             }
         }
 
@@ -68,26 +69,6 @@ public class WaterController : MonoBehaviour
     }
     public float GetHeightAtPosition(Vector3 position) 
     {
-        int index_x = Mathf.FloorToInt((position.x + WATER_SIZE / 2.0f) / SQUARE_SIZE);
-        int index_z = Mathf.FloorToInt((position.z + WATER_SIZE / 2.0f) / SQUARE_SIZE);
-
-        if (index_x < 0 || index_x >= WATER_VERTEX_WIDTH - 1 || index_z < 0 || index_z >= WATER_VERTEX_WIDTH - 1) {
-            return float.NaN;
-        }
-
-        Vector3 vertexTopleft = vertices[index_z * WATER_VERTEX_WIDTH + index_x];
-        Vector3 vertexBottomleft = vertices[(index_z + 1) * WATER_VERTEX_WIDTH + index_x];
-        Vector3 vertexTopright = vertices[index_z * WATER_VERTEX_WIDTH + (index_x + 1)];
-        Vector3 vertexBottomright = vertices[(index_z + 1) * WATER_VERTEX_WIDTH + (index_x + 1)];
-
-        float xDist1 = (position.x - vertexTopleft.x) / SQUARE_SIZE;
-        float xInterpolated1 = Mathf.Lerp(vertexTopleft.y, vertexTopright.y, xDist1);
-
-        float xDist2 = (position.x - vertexBottomleft.x) / SQUARE_SIZE;
-        float xInterpolated2 = Mathf.Lerp(vertexBottomleft.y, vertexBottomright.y, xDist2);
-
-        float interpolatedY = Mathf.Lerp(xInterpolated1, xInterpolated2, (position.z - vertexTopleft.z) / SQUARE_SIZE);
-
-        return interpolatedY;
+        return 20.0f * Mathf.PerlinNoise(position.x * 0.02f + timer, position.z * 0.02f + timer);
     }
 }
