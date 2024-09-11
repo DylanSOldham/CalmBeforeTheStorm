6e9723b83537bdd65f05054202d6b65697e83a52
RyukiAs <shionscars@gmail.com>
2024-09-11 16:33:21 -0500

    added text displaying storm state and countdown

--- Assets/Code/StormController.cs
+++ Assets/Code/StormController.cs
@@ -28,6 +28,19 @@ public class StormController : MonoBehaviour
             stormActive = !stormActive;
             timeUntilChange = DURATION;
         }
+
+
+        if(stormActive){
+            state.text = "Survive";
+
+        }else{
+            state.text = "Storm Countdown";
+
+        }
+
+        int timeRep = (int)timeUntilChange;
+        countDown.text = timeRep.ToString();
+
     }
 
     public bool isStormActive()
