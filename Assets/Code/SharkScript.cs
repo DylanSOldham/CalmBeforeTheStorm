4ce0c4ac4b126b7bd89dcf98612c98fb6630a37c
Brendo <brendo.coder@gmail.com>
2024-09-12 20:36:29 -0300

    dumb sharks

---
+++ Assets/Code/SharkScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class SharkScript : MonoBehaviour
+{
+    public WaterController waterController;
+    
+    private float _yOffset;
+    private const float RiseDuration = 3.0f;
+
+    private void Start()
+    {
+        StartCoroutine(RiseAnimation());
+        waterController = GameObject.Find("/Water").GetComponent<WaterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = transform.position;
+        newPosition.y = waterController.GetHeightAtPosition(newPosition) - _yOffset - 1;
+        transform.position = newPosition;
+        
+    }
+    
+    private IEnumerator RiseAnimation()
+    {
+        for (float t = 0; t < 1; t += Time.deltaTime / RiseDuration)
+        {
+            _yOffset = Mathf.Lerp(20, 0, t);
+            yield return null;
+        }
+    }
+}
